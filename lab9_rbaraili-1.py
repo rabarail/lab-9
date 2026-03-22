5e3e38bfc1ba1b6d69ee3751c65dcaf5d9f68632
Rajani <rajani.kami004@gmail.com>
2026-03-22 15:50:39 -0400

    import in main

--- lab9_rbaraili-1.py
+++ lab9_rbaraili-1.py
@@ -4,3 +4,6 @@ Author: RAJANI Baraili
 Purpose: This program implements the Match Coins Game, where two players toss coins each round, compete to match sides, and win or lose coins based on the outcome until the game ends.
 Date: March 22, 2026
 """
+import player as p
+import coin as c
+
