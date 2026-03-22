db98295c2a53e18e000e898d465826365746d4cc
Rajani <rajani.kami004@gmail.com>
2026-03-22 15:25:21 -0400

    define init

--- coin.py
+++ coin.py
@@ -4,3 +4,16 @@ Author: RAJANI BARAILI
 Purpose: This program implements the Match Coins Game, where two players toss coins each round, compete to match sides, and win or lose coins based on the outcome until the game ends.
 Date: March 22,2026
 """
+import random
+
+class Coin:
+
+    def _init_(self):
+        self.heads = "Heads"
+        self.tails = "Tails"
+
+
+
+
+
+
