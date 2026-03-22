6869eaff79741c5b8c380b0708c72c8630feb240
Rajani <rajani.kami004@gmail.com>
2026-03-22 18:24:53 -0400

    fixing

--- coin.py
+++ coin.py
@@ -1,5 +1,5 @@
 """
-Program Name: Match Coins Game - Player Class
+Program Name: Match Coins Game - Coin Class
 Author: RAJANI BARAILI
 Purpose: This program implements the Match Coins Game, where two players toss coins each round, compete to match sides, and win or lose coins based on the outcome until the game ends.
 Date: March 22,2026
@@ -9,14 +9,16 @@ import random
 class Coin:
 
     def __init__(self):
-        self.heads = "Heads"
-        self.tails = "Tails"
+        self.__sideup = 'Heads'
 
     def toss(self):
-        self.side_up = random.choice([self.head, self.tail])
+        if random.randint(0, 1) == 0:
+            self.__sideup = 'Heads'
+        else:
+            self.__sideup = 'Tails'
 
     def get_sideup(self):
-        return self.side_up
+        return self.__sideup
 
 
 
--- lab9_rbaraili-1.py
+++ lab9_rbaraili-1.py
@@ -15,6 +15,7 @@ def main():
     while True:
         play = input("Do you want to toss the coin? (y/n)")
         if play.lower() != 'y':
+            print("Tossing...")
             break
 
         player1.toss_coin()
@@ -23,17 +24,28 @@ def main():
         side1 = player1.get_coin_side()
         side2 =player2.get_coin_side()
 
-        print(f"{player1.get_name} tossed {side1}")
-        print(f"{player2.get_name} tossed {side2}")
+        print(f"{player1.get_name()} tossed {side1}")
+        print(f"{player2.get_name()} tossed {side2}")
 
         if side1 == side2:
             player1.win_coin()
             player2.lose_coin()
-            print(f"...No Match! {player1.get_name} wins a coin")
+            print(f"...Match! {player1.get_name()} wins a coin")
         else:
             player2.win_coin()
             player1.lose_coin()
-            print(f"...No Match! {player2.get_name} wins a coin")
+            print(f"...No Match! {player2.get_name()} wins a coin")
+
+        print(f"{player1.get_name()} has {player1.get_wallet()} coins")
+        print(f"{player2.get_name()} has {player2.get_wallet()} coins")
+
+
+        
+
+
+    
+
+main()
 
 
 
