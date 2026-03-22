fc81d777cf28cf807e538fd616ec7de5bad6761e
Rajani <rajani.kami004@gmail.com>
2026-03-22 15:13:01 -0400

    documentation for files

---
+++ coin.py
@@ -0,0 +1,6 @@
+"""
+Program Name: Match Coins Game - Player Class
+Author: RAJANI BARAILI
+Purpose: This program implements the Match Coins Game, where two players toss coins each round, compete to match sides, and win or lose coins based on the outcome until the game ends.
+Date: March 22,2026
+"""
---
+++ lab9_rbaraili-1.py
@@ -0,0 +1,6 @@
+"""
+Program Name: Match Coins Game - Main Game
+Author: RAJANI Baraili
+Purpose: This program implements the Match Coins Game, where two players toss coins each round, compete to match sides, and win or lose coins based on the outcome until the game ends.
+Date: March 22, 2026
+"""
